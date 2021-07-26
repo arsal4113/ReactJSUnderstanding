2966b59f42b5affd5523481fc02cb7d55f57ca11
unknown <arslaan4113@gmail.com>
2021-07-26 18:08:25 +0500

    use of axios for functional components

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-
+import React, { useEffect, useRef, useState, Component } from 'react';
+import Axios from "axios";
+/*
+  class componenets and browser's built in fetch to get the api results
+  */
 class App extends Component {
     state = {
         data: []
@@ -30,3 +33,41 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+  functional componenets and use of axios to get the api results
+  */
+  
+  export default function Images()
+  {
+	  const[images,setimages] - useState([]);
+	  
+	  const inputRef = useRef(null);
+	  
+	  const varRef = useRef(images.length);
+	  
+	  useEffect(()=> {
+		  
+		  inputRef.current.focus();
+		  
+		  Axios.get(
+		  
+		  "https://api.unsplash.com/photos/?client_id=F5c9bdjhihYqWahK0h3wixD_ZE9ujwIjw9nTAfdS_OM"
+		  
+		  ).then( (res)=> {
+			  
+			  setimages(res.data);
+		  });
+	  },[]);
+	  
+	  const [newImageUrl,setNewImageUrl] = useState("");
+	  
+	  
+	  function handleRemove(index){
+		  
+		  setimages([
+		  ...images.slice(0,index),
+		  ...images.slice(index + 1, images.length),
+		  ]);
+	  }
+  }
